31beaa005349c4b11332194073ec5a20535e2cb3
BattleBoi1992 <raineforest01@gmail.com>
2020-03-02 22:33:26 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+var scores, roundScore, activePlayer, gamePlaying;
+
+function initGame() {
+scores = [0,0]; 
+roundScore = 0; 
+activePlayer = 0; 
+gamePlaying = true;
+document.querySelector('.dice').style.display = 'none'; 
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('name-0').textContent = 'Player 1'; 
+document.getElementById('name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner')
+document.querySelector('.player-1-panel').classList.remove('winner')
+};
+
+initGame();
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if (gamePlaying) {
+        //random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+        //display result
+        var diceDOM = document.querySelector('.dice')
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'assets/dice-' + dice + '.png';
+        //update score if roll is not 1
+        if (dice !== 1) {
+            //add round score
+            roundScore += dice;
+            document.getElementById('current-' + activePlayer).textContent = roundScore; 
+        } else {
+            //switch active player
+            nextPlayer();
+        };
+    };
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        //add current score to active players global score
+        scores[activePlayer] += roundScore;
+        //update current player global score text
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        //check for winner
+        if (scores[activePlayer] >= 100) {
+            //clears the current round score text
+            document.getElementById('current-' + activePlayer).textContent = '0';
+            //adds winner style
+            document.getElementById('name-' + activePlayer).textContent = 'Winner!'
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+            //removes active player style
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            //removes the dice from the DOM
+            document.querySelector('.dice').style.display = 'none';
+            //disables roll dice
+            gamePlaying = false;
+        } else {
+            //switch active player
+        nextPlayer();
+        };
+    };
+});
+
+function nextPlayer() {
+    //set active player current score text to zero
+    document.getElementById('current-' + activePlayer).textContent = '0';
+    //switch active player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    //set round score back to zero
+    roundScore = 0; 
+    //change current player style
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    //remove dice from DOM
+    document.querySelector('.dice').style.display = 'none';
+};
+
+document.querySelector('.btn-new').addEventListener('click', initGame);
